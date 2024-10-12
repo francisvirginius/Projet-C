cf6e82c78d666df19e180b83f96f568cfc1f2b50
francisvirginius <francisvirginius@gmail.com>
2024-10-12 18:15:13 +0200

    Initial Commit

---
+++ Projet-C/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Projet-C
+//
+//  Created by francis virginius on 12/10/2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
